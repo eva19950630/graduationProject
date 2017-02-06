a94ee29a388cab65de2939f0c16fccbe4bc2e67b
eva19950630 <fun910347@gmail.com>
2017-02-06 16:24:45 +0800

    game_balance: UI modify and add SecondText_balance.cs

---
+++ Assets/Scripts/game_balance/SecondText_balance.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SecondText_balance : MonoBehaviour {
+	public Text sec;
+	public GameObject secText, label2, wrongsymbol, feedbackPanel, finishBtn;
+	public float ti = 0;
+	public int a = 0;
+	public Animator clockAni;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ti -= Time.deltaTime;
+		a = (int)ti + 150;
+		sec.text = ""+ a;
+		clockAni.enabled = true;
+		if (ti <= -141) {
+			sec.text = "0" + ""+ a;
+			sec.color = Color.red;
+			clockAni.Play ("clock_quick");
+		}
+		if (ti <= -150) {
+			wrongsymbol.SetActive(false);
+			secText.SetActive(false);
+			label2.SetActive(false);
+			finishBtn.SetActive(false);
+			clockAni.enabled = false;
+			
+			// feedbackPanel.SetActive(true);
+			// feedbackAni.Play("quickanswer_timesup");
+		}
+		if (ti <= -152) {
+			
+		}
+	}
+}
