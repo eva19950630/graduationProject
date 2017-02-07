94c692b4ea4af292e5c6b9ef9f0c4efc58c5fb63
RayRaya <xic768210@yahoo.com.tw>
2017-02-07 15:33:02 +0800

    worldII bulid the scene

---
+++ Assets/Scripts/WorldII_Data/savedataII.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class savedataII : MonoBehaviour {
+
+	public static savedataII saver;
+	public static bool gameEntered, comeback;
+
+	// Use this for initialization
+	void Start () {
+		//gameEntered = false;
+		comeback = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(gameventII.world_finish){			
+			// Destroy(this);
+			Destroy(this.gameObject);
+			// print("kill save");
+		}
+	}
+
+	void Awake(){
+
+		if(saver){
+			DestroyImmediate(gameObject);
+			comeback = true;
+		}else{
+		 	DontDestroyOnLoad(transform.gameObject);
+		 	saver = this;
+		}
+
+	}
+
+	public static void newround(){
+		gameEntered = false;
+	}
+
+	public static void entergame(){
+		gameEntered = true;
+	}
+
+}
