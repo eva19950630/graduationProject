9140a720aced2916dc51e1226328391c421678a7
eva19950630 <fun910347@gmail.com>
2017-02-16 13:15:22 +0800

    game_buying: UI modify and cancel the basket buttons

--- Assets/Scripts/game_buying/GameController_buying.cs
+++ Assets/Scripts/game_buying/GameController_buying.cs
@@ -4,10 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class GameController_buying : MonoBehaviour {
-	public Sprite[] basketSprite = new Sprite[2];
-	public SpriteRenderer[] mainren_basket = new SpriteRenderer[4];
-
-	private bool[] isClickBasket = new bool[4];
+	
 
 	// Use this for initialization
 	void Start () {
@@ -19,29 +16,5 @@ public class GameController_buying : MonoBehaviour {
 		
 	}
 
-	public void clickBasket(string btnName) {
-		if (btnName == "selectBasket1") {
-			isClickBasket[0] = true;;
-		}
-		else if (btnName == "selectBasket2") {
-			isClickBasket[1] = true;
-		}
-		else if (btnName == "selectBasket3") {
-			isClickBasket[2] = true;
-		}
-		else if (btnName == "selectBasket4") {
-			isClickBasket[3] = true;
-		}
-
-/*Only take one basket*/
-		for (int i = 0; i < 4; i++) {
-			if (isClickBasket[i] == true) {
-				mainren_basket[i].sprite = basketSprite[1];
-				isClickBasket[i] = false;
-			}
-			else {
-				mainren_basket[i].sprite = basketSprite[0];
-			}
-		}
-	}
+	
 }
