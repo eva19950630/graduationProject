ba21d6f4b0aaa3a37cceab3f232164c4eba51135
eva19950630 <fun910347@gmail.com>
2017-02-17 23:49:51 +0800

    game_buying: some preparation of game controller (to-do: draw objects, game controller)

--- Assets/Scripts/game_buying/SecondText_buying.cs
+++ Assets/Scripts/game_buying/SecondText_buying.cs
@@ -10,6 +10,7 @@ public class SecondText_buying : MonoBehaviour {
 
 	public float ti = 0;
 	public int a = 0;
+	public bool isboss = false;
 
 	private int gid;
 
@@ -17,31 +18,79 @@ public class SecondText_buying : MonoBehaviour {
 	void Start () {
 		gid = GameController_buying.gID;
 
+		if(GameObject.Find("BossSaveData"))
+			isboss = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		ti -= Time.deltaTime;
-		a = (int)ti + 180;
-		sec.text = ""+ a;
-		clockAni.enabled = true;
-		if (ti <= -171) {
-			sec.text = "0" + ""+ a;
-			sec.color = Color.yellow;
-			clockAni.Play ("clock_quick");
+		bool gamestate = GameController_buying.gamestate;
+		
+		if (isboss) {
+			if (gamestate == true) {
+				ti -= Time.deltaTime;
+				a = (int)ti + 90;
+				sec.text = ""+ a;
+				clockAni.enabled = true;
+				if (ti <= -81) {
+					sec.text = "0" + ""+ a;
+					sec.color = Color.yellow;
+					clockAni.Play ("clock_quick");
+				}
+				if (ti <= -90) {
+					wrongsymbol.SetActive(false);
+					secText.SetActive(false);
+					label2.SetActive(false);
+					finishBtn.SetActive(false);
+					clockAni.enabled = false;
+					
+					// feedbackPanel.SetActive(true);
+					// feedbackAni.Play("quickanswer_timesup");
+				}
+				if (ti <= -92) {
+					
+				}
+			} else {
+				if (ti <= -81)
+					sec.text = "0" + ""+ a;
+				else
+					sec.text = ""+ a;
+				
+				clockAni.enabled = false;
+			}
+		} else {
+			if (gamestate == true) {
+				ti -= Time.deltaTime;
+				a = (int)ti + 180;
+				sec.text = ""+ a;
+				clockAni.enabled = true;
+				if (ti <= -171) {
+					sec.text = "0" + ""+ a;
+					sec.color = Color.yellow;
+					clockAni.Play ("clock_quick");
+				}
+				if (ti <= -180) {
+					wrongsymbol.SetActive(false);
+					secText.SetActive(false);
+					label2.SetActive(false);
+					finishBtn.SetActive(false);
+					clockAni.enabled = false;
+					
+					// feedbackPanel.SetActive(true);
+					// feedbackAni.Play("quickanswer_timesup");
+				}
+				if (ti <= -182) {
+					
+				}
+			} else {
+				if (ti <= -171)
+					sec.text = "0" + ""+ a;
+				else
+					sec.text = ""+ a;
+				
+				clockAni.enabled = false;
+			}
 		}
-		if (ti <= -180) {
-			wrongsymbol.SetActive(false);
-			secText.SetActive(false);
-			label2.SetActive(false);
-			finishBtn.SetActive(false);
-			clockAni.enabled = false;
-			
-			// feedbackPanel.SetActive(true);
-			// feedbackAni.Play("quickanswer_timesup");
-		}
-		if (ti <= -182) {
-			
-		}
+
 	}
 }
