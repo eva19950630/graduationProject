e0c3b31d4b4ca2a36dd390383c4e5c2f2d56b20f
eva19950630 <fun910347@gmail.com>
2017-02-23 17:22:46 +0800

    user registered and save the data on DB (to-do: session, record format)

--- Assets/Scripts/UserDataSave.cs
+++ Assets/Scripts/UserDataSave.cs
@@ -2,17 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class UserDataSave : MonoBehaviour {
 	public string strurl;
 
 	public InputField accountField, passwordField;
+	public GameObject warningPanel, loadingImage, accountObj, passwordObj, accountLabel, passwordLabel;
+	public Slider loadingBar;
 
+	private AsyncOperation async;
 	private string user_name, user_passwd;
 
 	public static string username = "";
 	public static string password = "";
 	public static string result = "";
+
+	// public static bool account = false;
 
 	// Use this for initialization
 	void Start () {
@@ -24,25 +30,63 @@ public class UserDataSave : MonoBehaviour {
 		
 	}
 
+	public void clickSureBtn () {
+		warningPanel.SetActive(false);
+	}
+
 	public void saveToUserDB () {
-		user_name = accountField.text;
-		user_passwd = passwordField.text;
+		if (accountField.text == "" || passwordField.text == "") {
+			print("nothing");
+			warningPanel.SetActive(true);
+		} else {
+			user_name = accountField.text;
+			user_passwd = passwordField.text;
+			accountObj.SetActive(false);
+			passwordObj.SetActive(false);
+			accountLabel.SetActive(false);
+			passwordLabel.SetActive(false);
+			loadingImage.SetActive (true);
+			StartCoroutine(handlerEnterUser(user_name, user_passwd));
+		}
 		// print("[User] user_name: " + user_name + " / user_passwd: " + user_passwd);
-		StartCoroutine(handlerEnterUser(user_name, user_passwd));
 	}
 
 	IEnumerator handlerEnterUser (string user, string pass) {
-		string register_url = strurl + "?username=" + user + "&password=" + pass;
-		WWW entereddata = new WWW (register_url);
-		yield return entereddata;
-		if(entereddata.text == "registered") {
-			result = " correctly";
-		} else {
-			result = "incorrectly";
+		WWWForm form = new WWWForm();
+		Dictionary<string, string> data = new Dictionary<string, string>();
+		data.Add("username", user);
+		data.Add("password", pass);
+
+		foreach (KeyValuePair<string, string> post in data) {
+			form.AddField(post.Key, post.Value);
 		}
-		username = "";
-		password= "";
+
+		WWW www = new WWW(strurl, form);
+		yield return www;
+		Debug.Log(www.text);
+		StartCoroutine (LoadAndBar ("menu"));
     }
+
+    
+	IEnumerator LoadAndBar (string scene) {
+
+		float sProgress = 0f;
+		async = SceneManager.LoadSceneAsync(scene);
+		async.allowSceneActivation = false;
+		while(async.progress < 0.9f){
+			while(sProgress < async.progress){
+				sProgress += 0.01f;
+				loadingBar.value = sProgress;
+				yield return 0;
+			}
+		}
+		while(sProgress < 1f){
+				sProgress += 0.01f;
+				loadingBar.value = sProgress;
+				yield return 0;
+		}
+		async.allowSceneActivation = true;
+	}
 
 	
 }
