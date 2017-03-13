0619b9cb28b3223254dfa5221c6af6e251486eb6
RayRaya <xic768210@yahoo.com.tw>
2017-03-13 17:33:38 +0800

    build record panel and get user name successful.

---
+++ Assets/Scripts/menu/showRecords.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class showRecords : MonoBehaviour {
+
+	public string username;
+	public Text data_block;
+	// Use this for initialization
+	void Start () {
+		username = "";
+		username = UserDataSave.user_name;
+		// print(username);
+		data_block.text = username;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
